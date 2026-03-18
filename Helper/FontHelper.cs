1917224c8d590971152de3c038bee3322482bb95
Konstantin <konstant_z@yahoo.com>
2026-03-18 11:27:26 +0200

    Account Page View v2 all items

--- Helper/FontHelper.cs
+++ Helper/FontHelper.cs
@@ -12,5 +12,10 @@ namespace InClassWork.Helper
 		public const string CLOSED_EYE_ICON = "\ue8f5"; //Unicode for closed eye icon
 		public const string USERS_FILTER_ON = "\uef4f";
 		public const string USERS_FILTER_OFF = "\ueb32";
+		public const string DELETE_USER_ICON = "\ue872";
+		public const string LOGOUT_ICON = "\ue9ba";
+		public const string ADMIN_ICON = "\uf02e";
+		public const string HOME_ICON = "\ue88a";
+		public const string PERSON_ICON = "\ue7fd";
 	}
 }
--- ViewModels/AccountPageViewModel.cs
+++ ViewModels/AccountPageViewModel.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using InClassWork.Helper;
+using InClassWork.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,59 @@ using System.Threading.Tasks;
 
 namespace InClassWork.ViewModels
 {
-	public class AccountPageViewModel
+	public partial class AccountPageViewModel: ObservableObject
 	{
+		#region Properties
+		[ObservableProperty]
+		private string _firstName;
+
+		[ObservableProperty]
+		private string _lastName;
+
+		[ObservableProperty]
+		private string _userEmail;
+
+		[ObservableProperty]
+		private string _userMobile;
+
+		[ObservableProperty]
+		private AppUser _recievedUser; // Used to receive user details from the UsersListPage
+
+		[ObservableProperty]
+		private bool _isDeleteButtonVisible;
+
+		[ObservableProperty]
+		private string _deleteIcon;
+
+		[ObservableProperty]
+		private ImageSource _userImageSource;
+
+		[ObservableProperty]
+		private bool _errorMessageIsVisible;
+
+		[ObservableProperty]
+		private string _errorMessage;
+
+		[ObservableProperty]
+		private bool _isBusy;
+		#endregion
+
+
+		public AccountPageViewModel()
+		{
+			DeleteIcon = FontHelper.DELETE_USER_ICON;
+		}
+
+		[RelayCommand]
+		private void Delete()
+		{
+			// This command can be used to delete the user's account
+		}
+
+		[RelayCommand]
+		private void GetUserImage()
+		{
+			// This command can be used to fetch the user's profile image from the database
+		}
 	}
 }
